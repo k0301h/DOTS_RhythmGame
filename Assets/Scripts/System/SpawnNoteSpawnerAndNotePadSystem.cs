c6b25f973d6e1fb2d9fa752925f278c4c45fdb4d
inho.go(2019180003) <dlsghdmsqlc@naver.com>
2023-09-05 23:17:53 +0900

    초기 system 작성
    내일 일어나서 다시 하자

---
+++ Assets/Scripts/System/SpawnNoteSpawnerAndNotePadSystem.cs
@@ -0,0 +1,32 @@
+﻿using Unity.Burst;
+using Unity.Entities;
+
+namespace Game
+{
+    [BurstCompile]
+    public struct SpawnNoteSpawnerAndNotePadSystem : ISystem
+    {
+        [BurstCompile]
+        public void OnCreate(ref SystemState state)
+        {
+            state.Enabled = false;  // 무슨 의미?
+            var NotePadEntity = SystemAPI.GetSingletonEntity<NotePadAuthoring>();
+            var NotePad = SystemAPI.GetAspect<NotePadAspect>(NotePadEntity);
+            
+            var NoteSpawnerEntity = SystemAPI.GetSingletonEntity<NoteSpawnerAuthoring>();
+            var NoteSpawner = SystemAPI.GetAspect<NoteSpawnerAspect>(NoteSpawnerEntity);
+        }
+
+        [BurstCompile]
+        public void OnDestroy(ref SystemState state)
+        {
+            
+        }
+
+        [BurstCompile]
+        public void OnUpdate(ref SystemState state)
+        {
+            
+        }
+    }
+}
